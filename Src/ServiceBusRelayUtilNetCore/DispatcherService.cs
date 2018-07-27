6c269121186023e912f0681174a561c26208e9c8
Gabo Gilabert <ggilaber@microsoft.com>
2018-07-27 15:15:56 -0400

    First pass at a .Net Core version of the relay.

---
+++ Src/ServiceBusRelayUtilNetCore/DispatcherService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Relay;
+
+namespace GaboG.ServiceBusRelayUtilNetCore
+{
+    public class DispatcherService
+    {
+        private readonly string _connectionName;
+        private readonly string _key;
+        private readonly string _keyName;
+        private readonly string _relayNamespace;
+        private readonly string _targetServiceAddress;
+
+        public DispatcherService(string relayNamespace, string connectionName, string keyName, string key, string targetServiceAddress)
+        {
+            _relayNamespace = relayNamespace;
+            _connectionName = connectionName;
+            _keyName = keyName;
+            _key = key;
+            _targetServiceAddress = targetServiceAddress;
+        }
+
+        public async Task RunAsync()
+        {
+            var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(_keyName, _key);
+            var listener = new HybridConnectionListener(new Uri(string.Format("sb://{0}/{1}", _relayNamespace, _connectionName)), tokenProvider);
+            
+            // Subscribe to the status events.
+            listener.Connecting += Listener_Connecting;
+            listener.Offline += Listener_Offline;
+            listener.Online += Listener_Online;
+
+            // Provide an HTTP request handler
+            listener.RequestHandler = ListenerRequestHandler;
+
+            // Opening the listener establishes the control channel to
+            // the Azure Relay service. The control channel is continuously 
+            // maintained, and is reestablished when connectivity is disrupted.
+            await listener.OpenAsync();
+            Console.WriteLine("Server listening");
+
+            // Start a new thread that will continuously read the console.
+            await Console.In.ReadLineAsync();
+
+            // Close the listener after you exit the processing loop.
+            await listener.CloseAsync();
+        }
+
+        private void ListenerRequestHandler(RelayedHttpListenerContext context)
+        {
+            // Do something with context.Request.Url, HttpMethod, Headers, InputStream...
+            context.Response.StatusCode = HttpStatusCode.OK;
+            context.Response.StatusDescription = "OK";
+            using (var sw = new StreamWriter(context.Response.OutputStream))
+            {
+                sw.WriteLine("hello!");
+            }
+
+            // The context MUST be closed here
+            context.Response.Close();
+        }
+
+        private void Listener_Online(object sender, EventArgs e)
+        {
+            Console.WriteLine("Online");
+        }
+
+        private void Listener_Offline(object sender, EventArgs e)
+        {
+            Console.WriteLine("Offline");
+        }
+
+        private void Listener_Connecting(object sender, EventArgs e)
+        {
+            Console.WriteLine("Connecting");
+        }
+    }
+}
---
+++ Src/ServiceBusRelayUtilNetCore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+// https://docs.microsoft.com/en-us/azure/service-bus-relay/service-bus-relay-rest-tutorial
+// https://github.com/Azure/azure-relay-dotnet
+// https://docs.microsoft.com/en-us/azure/service-bus-relay/relay-hybrid-connections-http-requests-dotnet-get-started
+
+// This is what I think I need
+// https://github.com/Azure/azure-relay/blob/master/samples/hybrid-connections/dotnet/hcreverseproxy/README.md
+// https://github.com/Azure/azure-relay/tree/master/samples/hybrid-connections/dotnet/hcreverseproxy
+
+// Publish
+// https://stackoverflow.com/questions/44074121/build-net-core-console-application-to-output-an-exe
+// https://docs.microsoft.com/en-us/dotnet/core/rid-catalog
+
+namespace GaboG.ServiceBusRelayUtilNetCore
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            var relayNamespace = Configuration["RelayNamespace"];
+            var connectionName = Configuration["ConnectionName"];
+            var keyName = Configuration["KeyName"];
+            var key = Configuration["Key"];
+            var targetServiceAddress = Configuration["TargetServiceAddress"];
+
+            var service = new DispatcherService(relayNamespace, connectionName, keyName, key, targetServiceAddress);
+            service.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
